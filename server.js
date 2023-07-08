7e093f21cb02e028dc6874e3d881a49b8ed36d8e
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-08 00:24:01 -0400

    updated .env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const prompts = require('./db/prompts');
 const express = require('express');
 const mysql = require('mysql2');
 
-const port = process.env.port || 3001;
+const port = process.env.DA_PORT || 3001;
 const app = express();
 
 app.use(express.json());
@@ -15,3 +15,5 @@ const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+
+
