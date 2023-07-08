881faeb41b33cc606784a0ffa9049ae8c6bb2d9a
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-07 23:58:02 -0400

    initialized variables in server.js

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 const prompts = require('./db/prompts');
 
+const express = require('express');
+const mysql = require('mysql2');
+
+const port = process.env.port || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password here
+        password: 'Haachama18!',
+        database: 'movies_db'
+    }
+)
