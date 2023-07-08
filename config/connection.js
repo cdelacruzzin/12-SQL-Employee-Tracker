7d3115ccbed2e66c15000de5b2b4eb82dd067dd1
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-08 00:20:47 -0400

    added the connection.js code into the server.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,10 @@
-const Sequelize = require('sequelize');
-let sequelize;
+const mysql = require('mysql2');
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-module.exports = sequelize;
+module.exports = connection;
--- server.js
+++ server.js
@@ -9,13 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // MySQL username,
-        user: 'root',
-        // TODO: Add MySQL password here
-        password: 'Haachama18!',
-        database: 'movies_db'
-    }
-)
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
