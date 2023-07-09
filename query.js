dd94ab2ddfb2a7406d9250511090c4b6c0bd9178
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-08 21:02:09 -0400

    added a function to update employee

--- query.js
+++ query.js
@@ -23,52 +23,83 @@ function viewRoles() {
 
 function viewEmployee() {
     const sql = `SELECT * FROM employee`;
-    db.query(sql, (err, rows) => console.table(rows));  
+    db.query(sql, (err, rows) => console.table(rows));
 }
 
-function addDept() {
+function addDept(values) {
 
-    const values = `8, "chef"`;
+    values = `8, "chef"`;
     const sql = `INSERT INTO department (id, department_name)
     VALUES (${values})`;
 
-    db.query(sql, (err, rows) => console.log(`successfully added (${values}) to database`));  
+    db.query(sql, (err, rows) => console.log(`successfully added (${values}) to database`));
 }
 
-function addRole() {
+function addRole(values) {
 
-    const values = `12, "head chef", 6000.00, 8`;
+    values = `12, "head chef", 6000.00, 8`;
     const sql = `INSERT INTO role (id, title, salary, department_id)
     VALUES (${values})`;
 
-    db.query(sql, (err, rows) => console.log(`successfully added (${values}) to database`));  
+    db.query(sql, (err, rows) => console.log(`successfully added (${values}) to database`));
 }
 
-function addEmployee() {
+function addEmployee(values) {
 
-    const values = `5, 'pawa', 'raca', 8, 2`;
-    const sql = `INSERT INTO role employee (id,  first_name, last_name, role_id, manager_id)
+    values = `5, 'pawa', 'raca', 8, 2`;
+    const sql = `INSERT INTO employee (id,  first_name, last_name, role_id, manager_id)
     VALUES (${values})`;
 
-    db.query(sql, (err, rows) => console.log(`successfully added (${values}) to database`));  
+    db.query(sql, (err, rows) => console.log(`successfully added (${values}) to database`));
 }
 
+function updateEmployeeRole() {
+
+    const role_id = 3;
+    const id = 1;
+
+    const sql = `SELECT * FROM employee WHERE id = ?`;
+
+    db.query(sql, id, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        if (result == "") {
+            console.log('this id does not exist');
+            return;
+        }
+
+        const sql = `SELECT * FROM role WHERE id = ?`;
+        db.query(sql, role_id, (err, result) => {
+            // console.log(result);
+            if (err) {
+                console.log(err);
+            }
+            if (result == "") {
+                console.log('this role id does not exist');
+                return;
+            }
+
+            const sql =  `UPDATE employee SET role_id = ? WHERE id = ?`;
+            db.query(sql, [role_id, id], (err, result) => {
+                if (err) {
+                    console.log(err);
+                } 
+                console.log('Employee role updated successfully.');
+            });
+        });
+
+    });
+}
+
+// viewEmployee();
+updateEmployeeRole();
+// viewRoles();
+// viewDep();
+// viewEmployee();
 
 
-
-
-
-addEmployee();
-
-
-
-
-
-
-
-viewRoles();
-viewDep();
-viewEmployee();
+module.exports = { viewDep, viewRoles, viewEmployee, addDept, addRole, addEmployee }
 
 
 
