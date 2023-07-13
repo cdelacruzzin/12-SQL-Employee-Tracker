4a04855f447288a31ee039a928efb4436679d72e
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-13 12:56:48 -0400

    added semicolons;

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
 -- initial values for tables
 INSERT INTO department (id, department_name)
-VALUES (1, "IT")
+VALUES (1, "IT");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (1, 'Software Developer', 90000.00, 1),
+VALUES (1, 'Software Developer', 90000.00, 1);
 
 
 INSERT INTO employee (id,  first_name, last_name, role_id, manager_id)
-VALUES  (1, 'carlos', 'dela cruz', 1, NULL),  -- manager
+VALUES  (1, 'carlos', 'dela cruz', 1, NULL);  -- manager
 
